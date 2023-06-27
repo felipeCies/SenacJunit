c2fa758657a95ba56973e0d58a5d312a08763927
felipeCies <cieslick246@gmail.com>
2023-06-26 21:26:14 -0300

    commit de ajustes de testes

--- src/main/java/com/senac/PositivoNegativo.java
+++ src/main/java/com/senac/PositivoNegativo.java
@@ -2,12 +2,12 @@ package com.senac;
 
 public class PositivoNegativo { public static String avalia(double n1){
     if (n1>0){
-        return "positivo";
+        return "Positivo";
     } else if (n1<0) {
-        return "negativo";
+        return "Negativo";
     }
     else {
-        return "zero";
+        return "Zero";
     }
 }
 }
